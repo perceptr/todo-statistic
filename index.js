6af150b532f5a756de174e736bd306abf2880729
Pavel Reshetnikov <pasha.keyzet@yandex.ru>
2023-03-06 17:44:00 +0500

    alltodos

--- index.js
+++ index.js
@@ -2,13 +2,26 @@ const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
 const {readLine} = require('./console');
 
 const files = getFiles();
+console.log(getAllToDos())
 
 console.log('Please, write your command!');
-readLine(processCommand);
 
 function getFiles() {
     const filePaths = getAllFilePathsWithExtension(process.cwd(), 'js');
     return filePaths.map(path => readFile(path));
+}
+
+function getAllToDos() {
+    let todos = [];
+    for (const file of files) {
+        for (const line of file.split("\n")) {
+            const index = line.indexOf('// TODO ');
+            if (index !== -1) {
+                todos.push(line.slice(index));
+            }
+        }
+    }
+    return todos;
 }
 
 function processCommand(command) {
